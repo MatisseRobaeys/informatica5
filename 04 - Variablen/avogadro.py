8da3d1199ad56f4d80ac8f7b33574505a01f3c05
MatisseRobaeys <43027767+MatisseRobaeys@users.noreply.github.com>
2018-09-27 18:54:45 +0200

    avogadro

---
+++ 04 - Variablen/avogadro.py
@@ -0,0 +1,9 @@
+aantal_mol_s = float(input('aantal deeltjes: '))
+constante_van_avogadro = 6.020 * 10**23
+molaire_massa_zwavel = 32.06
+
+resultaat_1 = aantal_mol_s * molaire_massa_zwavel
+resultaat_2 = aantal_mol_s * constante_van_avogadro
+
+print(resultaat_1)
+print(resultaat_2)
