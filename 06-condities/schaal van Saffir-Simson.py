2b8db0eedf4807e8044ed324ce41fb25eb9138a5
MatisseRobaeys <43027767+MatisseRobaeys@users.noreply.github.com>
2018-10-13 10:53:39 +0200

    avogad

---
+++ 06-condities/schaal van Saffir-Simson.py
@@ -0,0 +1,14 @@
+ windsnelheid =float(input('windsnelheid: '))
+#berekening van categorie
+if windsnelheid < 119:
+    uitvoer = 'geen orkaan'
+elif windsnelheid >= 119 and windsnelheid <= 153:
+    uitvoer = 'categorie 1'
+elif windsnelheid >= 154 and windsnelheid <= 177:
+    uivoer = 'categorie 2'
+elif windsnelheid >= 178 and windsnelheid <= 209:
+    uitvoer = 'categorie 3'
+elif windsnelheid >= 210 and windsnelheid <= 249:
+    uitvoer = 'categorie 4'
+else:
+    uitvoer = 'categorie 5'
