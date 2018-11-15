216ae0a8510881f38584c1c4a48c6122846aebcb
MatisseRobaeys <43027767+MatisseRobaeys@users.noreply.github.com>
2018-11-15 09:23:27 +0100

    avogad

---
+++ 07-iteraties-for-lus/omkeren.py
@@ -0,0 +1,6 @@
+woord = (input('woord : '))
+omgekeerde = ''
+for letter in woord:
+    omgekeerde += letter
+
+    print(omgekeerde)
---
+++ 07-iteraties-for-lus/test.py
@@ -0,0 +1,2 @@
+for i in range (13):
+    print('Ik zal meer dan twee oefeningen programmeren maken!')
