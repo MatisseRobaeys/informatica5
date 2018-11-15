cbf153ddf528d3e13da26d8c68109626daa1e085
MatisseRobaeys <43027767+MatisseRobaeys@users.noreply.github.com>
2018-11-15 15:48:42 +0100

    avogad

---
+++ 07-iteraties-for-lus/rad van u ma.py
@@ -0,0 +1,12 @@
+prijzengeld = 0
+woord = input('woord?: ')
+geld = float(input('gedraaid: '))
+letter = input('letter?: ')
+raden = ''
+
+while (letter in woord) and (letter not in raden):
+    prijzengeld += geld
+    raden += letter
+    letter = input('letter?: ')
+
+print(prijzengeld
